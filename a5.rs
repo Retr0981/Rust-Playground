da7d28b8ac56239af7ed4efcfd2e2a173877ee16
Retr0981 <davidarmah991@gmail.com>
2023-08-21 10:43:21 +0100

    Adding looping examples

---
+++ a5.rs
@@ -0,0 +1,22 @@
+// Topic: Looping using the loop statement
+//
+// Program requirements:
+// * Display "1" through "4" in the terminal
+//
+// Notes:
+// * Use a mutable integer variable
+// * Use a loop statement
+// * Print the variable within the loop statement
+// * Use break to exit the loop
+
+fn main() {
+    let mut i = 1;
+
+    loop {
+        println!("{:?}", i);
+        i = i + 1;
+        if i == 5 {
+            break;
+        }
+    }
+}
---
+++ a6.rs
@@ -0,0 +1,24 @@
+// Topic: Looping using the while statement
+//
+// Program requirements:
+// * Counts down from 5 to 1, displays the countdown
+//   in the terminal, then prints "done!" when complete.
+//
+// Notes:
+// * Use a mutable integer variable
+// * Use a while statement
+// * Print the variable within the while loop
+// * Do not use break to exit the loop
+
+fn main() {
+    let mut i = 5;
+
+    while i >= 1 {
+        println!("{:?}", i);
+        i -= 1;
+        if i == 0 {
+            println!("done");
+            break;
+        }
+    }
+}
