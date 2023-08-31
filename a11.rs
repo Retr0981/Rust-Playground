a62341a358a1ff13b31771efdd68905202c25fa2
Retr0981 <davidarmah991@gmail.com>
2023-08-31 14:40:34 +0100

    adding some rusty code

---
+++ a11.rs
@@ -0,0 +1,33 @@
+// Topic: Ownership
+//
+// Requirements:
+// * Print out the quantity and id number of a grocery item
+//
+// Notes:
+// * Use a struct for the grocery item
+// * Use two i32 fields for the quantity and id number
+// * Create a function to display the quantity, with the struct as a parameter
+// * Create a function to display the id number, with the struct as a parameter
+
+struct GroceryItem {
+    quantity: i32,
+    id: i32,
+}
+
+fn display_quantity(item: &GroceryItem) {
+    println!("quantity : {:?}", item.quantity)
+}
+
+fn display_id(item: &GroceryItem) {
+    println!("id : {:?}", item.id)
+}
+
+fn main() {
+    let item = GroceryItem {
+        quantity: 20,
+        id: 3,
+    };
+
+    display_quantity(&item);
+    display_id(&item);
+}
